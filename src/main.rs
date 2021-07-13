f069eaf3df626c319d05f61fc17619c9af804636
LonnonjamesD <xxl3m0nk1ngxx@gmail.com>
2021-07-13 02:22:25 -0400

    :mailbox: Framework

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,19 @@
+mod token;
+use crate::token::*;
+mod economy;
+use crate::economy::ECONOMY_GROUP;
+use std::env;
 use serenity::{
     async_trait,
-    model::{channel::Message, gateway::Ready},
-    prelude::*,
+    model::gateway::Ready,
 };
+use serenity::client::{Client, Context, EventHandler};
+use serenity::framework::standard::StandardFramework;
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {:?}", why);
-            }
-        }
-    }
-
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
@@ -41,17 +24,27 @@ async fn main() {
     // Configure the client with your Discord bot token in the environment.
     let args: Vec<String> = env::args().collect();
     let token: &str;
+    let prefix: Vec<&str>;
     if args[1] == "beta" {
         token = BETA_TOKEN;
+        prefix = vec!["bluh", "Bluh"]
     } else {
         token = STABLE_TOKEN;
+        prefix = vec!["glub", "Glub"]
     }
 
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
-    let mut client =
-        Client::builder(&token).event_handler(Handler).await.expect("Err creating client");
+    let framework = StandardFramework::new()
+    .configure(|c| c.prefixes(prefix))
+    // The `#[group]` (and similarly, `#[command]`) macro generates static instances
+    // containing any options you gave it. For instance, the group `name` and its `commands`.
+    // Their identifiers, names you can use to refer to these instances in code, are an
+    // all-uppercased version of the `name` with a `_GROUP` suffix appended at the end.
+    .group(&ECONOMY_GROUP);
+
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await.expect("Err creating client");
 
     // Finally, start a single shard, and start listening to events.
     //
