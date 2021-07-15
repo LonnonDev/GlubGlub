3981655aa266d01f5c9fbb843c31b47502616349
LonnonjamesD <xxl3m0nk1ngxx@gmail.com>
2021-07-15 11:48:09 -0400

    :arrow_up: Tokens/Improvements

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 mod token;
 use crate::token::*;
-mod economy;
-use crate::economy::ECONOMY_GROUP;
+mod commands;
+use crate::commands::economy::ECONOMY_GROUP;
+use crate::commands::owner::OWNER_GROUP;
 use std::env;
 use serenity::{
     async_trait,
     model::gateway::Ready,
 };
+use serenity::framework::standard::{
+    StandardFramework,
+    help_commands,
+    Args,
+    HelpOptions,
+    CommandGroup,
+    CommandResult,
+};
 use serenity::client::{Client, Context, EventHandler};
-use serenity::framework::standard::StandardFramework;
+use serenity::framework::standard::macros::help;
+use serenity::model::prelude::{Message, UserId};
+use std::collections::HashSet;
+use serenity::framework::standard::WithWhiteSpace;
+use serenity::prelude::*;
+
+pub mod useful;
+
+struct Framework;
+
+impl TypeMapKey for Framework {
+    type Value = StandardFramework;
+}
 
 struct Handler;
 
@@ -19,37 +40,51 @@ impl EventHandler for Handler {
     }
 }
 
+#[help]
+async fn my_help(
+   context: &Context,
+   msg: &Message,
+   args: Args,
+   help_options: &'static HelpOptions,
+   groups: &[&'static CommandGroup],
+   owners: HashSet<UserId>
+) -> CommandResult {
+    let _ = help_commands::with_embeds(context, msg, args, help_options, groups, owners).await;
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() {
-    // Configure the client with your Discord bot token in the environment.
+    
     let args: Vec<String> = env::args().collect();
     let token: &str;
     let prefix: Vec<&str>;
     if args[1] == "beta" {
+        println!("Beta Bot");
         token = BETA_TOKEN;
         prefix = vec!["bluh", "Bluh"]
-    } else {
+    } else if args[1] == "stable" {
+        println!("Stable Bot");
         token = STABLE_TOKEN;
         prefix = vec!["glub", "Glub"]
+    } else {
+        panic!("`{}` is not a valid option for `bot type`, `stable` and `beta` are valid options", args[1])
     }
 
     let framework = StandardFramework::new()
-    .configure(|c| c.prefixes(prefix))
-    // The `#[group]` (and similarly, `#[command]`) macro generates static instances
-    // containing any options you gave it. For instance, the group `name` and its `commands`.
-    // Their identifiers, names you can use to refer to these instances in code, are an
-    // all-uppercased version of the `name` with a `_GROUP` suffix appended at the end.
-    .group(&ECONOMY_GROUP);
+        .configure(|c| c.with_whitespace(WithWhiteSpace::from(true)))
+        .configure(|c| c.prefixes(prefix))
+        .configure(|c| c
+        .owners(vec![UserId(859806257774723102)].into_iter().collect()))
+        .help(&MY_HELP)
+        .group(&ECONOMY_GROUP)
+        .group(&OWNER_GROUP);
 
     let mut client = Client::builder(&token)
         .event_handler(Handler)
         .framework(framework)
         .await.expect("Err creating client");
 
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
