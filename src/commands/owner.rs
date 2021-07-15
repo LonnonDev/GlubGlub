8fb013de7f1f7cd4e5a6ddb582365714f5dbd8a0
LonnonjamesD <xxl3m0nk1ngxx@gmail.com>
2021-07-15 11:47:08 -0400

    :white_check_mark: Panic/Stop bot Command

---
+++ src/commands/owner.rs
@@ -0,0 +1,20 @@
+use serenity::client::Context;
+use serenity::model::channel::Message;
+use serenity::framework::standard::macros::{command, group};
+use serenity::framework::standard::CommandResult;
+use std::process;
+use crate::useful::*;
+
+//* Panic command
+//* Stops the bot
+#[command]
+async fn panic(ctx: &Context, msg: &Message) -> CommandResult {
+    sendmessage("Stopping the Bot...", ctx, msg).await;
+    println!("Bluh `panic` command was ran\nStopping Bot...");
+    process::exit(0);
+}
+
+#[group]
+#[commands(panic)]
+#[owners_only]
+struct Owner;
