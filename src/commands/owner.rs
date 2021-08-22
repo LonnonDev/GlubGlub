8b5710ca06d4d7d52c41090df4794c64cdccbbf6
LonnonjamesD <xxl3m0nk1ngxx@gmail.com>
2021-08-22 19:57:16 -0400

    test

--- src/commands/owner.rs
+++ src/commands/owner.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use serenity::client::Context;
 use serenity::model::channel::Message;
 use serenity::framework::standard::macros::{command, group};
@@ -14,8 +15,21 @@ async fn panic(ctx: &Context, msg: &Message) -> CommandResult {
     process::exit(0);
 }
 
+
+#[command]
+async fn author_exile_test(ctx: &Context, msg: &Message) -> CommandResult {
+    let mut ids = vec![];
+    for _ in 0..10 {
+        let rand_id = rand::thread_rng().gen_range(111111111111111111u128..999999999999999999u128);
+        ids.push(rand_id);
+        ids.push(rand_id % 4);
+    }
+    sendmessage(format!("{:#?}", ids).as_str(), ctx, msg).await;
+    Ok(())
+}
+
 #[group]
-#[commands(panic)]
+#[commands(panic, author_exile_test)]
 #[only_in("guilds")]
 #[owners_only]
 pub struct Owner;
